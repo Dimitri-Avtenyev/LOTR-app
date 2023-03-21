63b8f9ff67c0bf7c8eb0a3c025e4f481b39491b0
Michiel Baert <michiel.baert@student.ap.be>
2023-03-21 16:45:26 +0100

    add startpage code

--- client/src/Startpage/Startpage.tsx
+++ client/src/Startpage/Startpage.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styles from "./Startpage.module.css";
+import Button from 'react-bootstrap/Button';
 import { Quote } from "../types";
 
 
@@ -17,9 +18,13 @@ const Startpage = () => {
 
   return (
     <div>
-      <h1>Lord of the rings Quiz</h1>
-      <p>uitleg</p>
-      <button></button>
+      <main>
+          <div className={styles.startpage}>
+              <h1>Lord of the rings <br></br> Quiz</h1>
+              <p>Beetje uitleg over de quiz</p>
+              <Button className={styles.startButton} variant="primary" size="lg">Start Quiz</Button>
+          </div>
+      </main>
       {quotes.map((quote:Quote) => { // test 10 random quotes
         return (
           <p key={quote.id}>
