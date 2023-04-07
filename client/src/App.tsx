834ab9937b45fde671b02197e3edd145c141bdb8
Dimitri Avtenyev <dimitri.avtenyev@student.ap.be>
2023-04-07 16:05:50 +0200

    added user-page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,13 +13,15 @@ import EndQuizPage from './Components/EndQuizPage/EndQuizPage';
 import ErrorPage from './Components/ErrorPage/ErrorPage';
 import LoginPage from './Components/LoginPage/LoginPage';
 import ResultPage from './Components/ResultPage/ResultPage';
+import AccountPage from './Components/AccountPage/AccountPage';
 
 const App = () => {
   const [loggedin, setLoggedin] = useState<boolean>(JSON.parse(localStorage.getItem("loggedin")?? "false"));
   const [user, setUser] = useState<User>({
     _id: "123",
-    userName: "Guest",
-    highscore: 0,
+    avatarID: 1,
+    userName: "Gandalf",
+    highscore: 10,
     favorites: [],
     blacklist: []
   });
@@ -41,19 +43,23 @@ const App = () => {
           element: <LoginPage />
         },
         {
-          path: "startpage/:project", 
+          path: "account",
+          element: <AccountPage account={user}/>
+        },
+        {
+          path: "start/:project", 
           element: <Startpage />
         },
         {
-          path: "quizpage/theone",
+          path: "quiz/theone",
           element: <Quizpage limit={1} />
         },
         {
-          path: "quizpage/result",
+          path: "quiz/result",
           element: <ResultPage />
         },
         {
-          path: "quizpage/endquizpage",
+          path: "quiz/endquiz",
           element: <EndQuizPage />
         }
       ]
--- client/src/types.tsx
+++ client/src/types.tsx
@@ -1,5 +1,6 @@
 export interface User {
   _id?:           string;
+  avatarID:       number;
   userName:       string;
   highscore:      number;
   favorites:      Favorite[];
