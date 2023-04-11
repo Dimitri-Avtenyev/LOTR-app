2feae3f02463c8895c9ed817248dc6de4aba07ce
Michiel Baert <michiel.baert@student.ap.be>
2023-04-11 18:02:56 +0200

    rewrite quiz logic

--- client/src/Components/Quizpage/Quizpage.tsx
+++ client/src/Components/Quizpage/Quizpage.tsx
@@ -2,112 +2,66 @@ import React, { useEffect, useState } from "react";
 import styles from "./Quizpage.module.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LoadingIndicator from "../LoadingIndicator/LoadingIndicator";
+import ResultPage from "../ResultPage/ResultPage";
 import { Quote } from "../../types";
+import { act } from "react-dom/test-utils";
 
-interface QuizProps {
-    limit: number
-}
-
-const Quizpage = ({limit = 1} : QuizProps) => {
-    const [quotes, setQuotes] = useState<Quote[]>([]);
+const Quizpage = () => {
     const [loading, setLoading] = useState(false);
+    const [quotes, setQuotes] = useState<Quote[]>([]);
+    const [activeQuestion, setActiveQuestion] = useState(0);
+    const [selectedAnswer, setSelectedAnswer] = useState(false);
+    const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(0);
 
     useEffect(() => {
         setLoading(true);
-        const loadQuotes = async (limit : number) => {
-        let response = await fetch("http://localhost:3000/api/quiz/" + limit);
+        const loadQuotes = async () => {
+        let response = await fetch("http://localhost:3000/api/quiz");
         let data: Quote[] = await response.json();
         setQuotes(data);
         setLoading(false);
         }
-        loadQuotes(limit);
-        
-    }, [limit])
+        loadQuotes();
+    }, [])
+    
+    const submitAnswerHandler = () => {
+        setActiveQuestion((prev) => prev + 1)
+    }
 
-    let characterArray : string[] = [];
-    quotes.map((quote : Quote) => {
-        return (
-            characterArray.push(quote.character.name),
-            characterArray.push(quote.wrongAnswers.character[0].name),
-            characterArray.push(quote.wrongAnswers.character[1].name)
-        )
-    });
-
-    let movieArray : string[] = [];
-    quotes.map((quote : Quote) => {
-        return (
-            movieArray.push(quote.movie.name),
-            movieArray.push(quote.wrongAnswers.movie[0].name),
-            movieArray.push(quote.wrongAnswers.movie[1].name)
-        )
-    })
-
-    const shuffleArray = (array : string[]) => {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i+1));
-            const temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
+    const onAnswerSelected = (answer : string, index : number) => {
+        setSelectedAnswerIndex(index)
+        if(answer === quotes[activeQuestion].character.name) {
+            setSelectedAnswer(true)
+        } else {
+            setSelectedAnswer(false)
         }
     }
 
-    shuffleArray(characterArray);
-    shuffleArray(movieArray);
-
-   
     return (
-        
         <main className={styles.main}>
             {loading && <LoadingIndicator/>}
             
-            {quotes.map((quote: Quote) => {
-                return (
-                    <div>
-                        <h3>1/10</h3>
-                        <h3>Quote: {quote.dialog}</h3>
-                    </div>
-                )
-            })}
+            <div>
+                <h3>{activeQuestion}/10</h3>
+                <h3>Quote: {quotes[activeQuestion].dialog}</h3>
+            </div>
+                
             <div className={styles.quizForm}>
                 <div className={styles.columnLeft}>
-                    {characterArray.map((character : string) => {
-                        return (
-                            <p>{character}</p>
-                        )
-                    })}
+                    <p>{quotes[activeQuestion].character.name}</p>
+                    <p>{quotes[activeQuestion].wrongAnswers.character[0].name}</p>
+                    <p>{quotes[activeQuestion].wrongAnswers.character[1].name}</p>
                 </div>
                 <div className={styles.line}></div>
                 <div className={styles.columnRight}>
-                    {movieArray.map((movie : string) => {
-                        return (
-                            <p>{movie}</p>
-                        )
-                    })}
+                    <p>{quotes[activeQuestion].movie.name}</p>
+                    <p>{quotes[activeQuestion].wrongAnswers.movie[0].name}</p>
+                    <p>{quotes[activeQuestion].wrongAnswers.movie[1].name}</p>
                 </div>
             </div>
-            <button className={styles.submitButton}>Submit Answer</button>
+            <button className={styles.submitButton} onClick={submitAnswerHandler}>Submit Answer</button>
         </main>
     )
 }
- /*
-<div>
-                        <h3>1/10</h3>
-                        <h3>Quote: Dialog</h3>
-                        
-                        <div className={styles.quizForm}>
-                            <div className={styles.columnLeft}>
-                                <p>Character</p>
-                                <p>Character</p>
-                                <p>Character</p>
-                            </div> 
-                            <div className={styles.line}></div>
-                            <div className={styles.columnRight}>
-                                <p>Movie</p>
-                                <p>Movie</p>
-                                <p>Movie</p>
-                            </div>
-                        </div>
-                    </div>
-*/
 
 export default Quizpage;
