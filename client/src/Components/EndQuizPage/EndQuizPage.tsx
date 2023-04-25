9c64aa407454975ea800b6518b56fe4138c97d34
Michiel Baert <michiel.baert@student.ap.be>
2023-04-25 20:41:32 +0200

    changes to resultpage

--- client/src/Components/EndQuizPage/EndQuizPage.tsx
+++ client/src/Components/EndQuizPage/EndQuizPage.tsx
@@ -7,10 +7,10 @@ const EndQuizPage = () => {
     return (
         <main>
             <div className={styles.container}>
-            <h1>End Quiz</h1>
-            <p>Your score: X/10</p>
-            <p>Your highscore: X/10</p>
-            <Link to="/startpage/theone">Try again?</Link>
+                <h1>End Quiz</h1>
+                <p>Your score: X/10</p>
+                <p>Your highscore: X/10</p>
+                <Link to="/start/:project">Try again?</Link>
             </div>
         </main>
        ) 
