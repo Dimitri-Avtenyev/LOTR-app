05cc949f782d956e7cb5c4fc5735186f10128282
Gwen Sneijders <gwen.sneijders@ap.be>
2023-04-25 18:56:51 +0200

    footer aanpassen

--- client/src/Components/Footer/Footer.tsx
+++ client/src/Components/Footer/Footer.tsx
@@ -1,9 +1,14 @@
 import styles from "./Footer.module.css";
+import APLogo from "./assets/AP_logo.png";
+import githubLogo from "./assets/githubLogo.png";
+import ringLogo from "./assets/thering.png";
 
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <p>copyright</p>
+            <p>&#169; In opdracht van <img src={APLogo} alt="AP_logo"  height="15px"/></p>
+            <img src={ringLogo} alt="ring_logo" height="45px" />
+            <a href="https://github.com/Dimitri-Avtenyev/LOTR-app">Find us on github <img src={githubLogo} alt="github_logo" height="25px" /></a>   
         </footer>
     )
 }
